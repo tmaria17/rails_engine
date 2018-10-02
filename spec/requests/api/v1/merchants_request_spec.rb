5ee6c46900e30b04ab3308a40c6e2964948473b4
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-02 14:07:12 -0600

    Adds test for merchants index endpoint and merchant show endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -6,6 +6,20 @@ describe 'Merchants api' do
 
     get '/api/v1/merchants'
 
+    merchants = JSON.parse(response.body)
+
     expect(response).to be_successful
+    expect(merchants.count).to eq(3)
+  end
+
+  it 'finds one merchant'do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
   end
 end
