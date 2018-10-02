63cf0398b8416a0f3b7fb600f298e0ff349d5745
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-02 17:51:56 -0600

    Creates invoice items migration and model. Imports invoice items.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_10_02_232341) do
-
-
+ActiveRecord::Schema.define(version: 2018_10_02_234341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +22,16 @@ ActiveRecord::Schema.define(version: 2018_10_02_232341) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "invoice_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "unit_price"
+    t.bigint "invoice_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
+    t.index ["item_id"], name: "index_invoice_items_on_item_id"
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.string "status"
@@ -35,7 +42,6 @@ ActiveRecord::Schema.define(version: 2018_10_02_232341) do
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
     t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
-
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -53,6 +59,8 @@ ActiveRecord::Schema.define(version: 2018_10_02_232341) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
