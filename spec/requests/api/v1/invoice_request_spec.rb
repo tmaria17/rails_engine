533f3facb616d4e714bf720dfbb256441504c94d
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-03 15:08:59 -0600

    Finishes testing for invoice endpoints. ATP.

--- spec/requests/api/v1/invoice_request_spec.rb
+++ spec/requests/api/v1/invoice_request_spec.rb
@@ -16,14 +16,17 @@ describe 'invoices api' do
     expect(invoices.count).to eq(2)
   end
 
-  # it 'finds one invoice'do
-  #   id = create(:invoice).id
-  #
-  #   get "/api/v1/invoices/#{id}"
-  #
-  #   invoice = JSON.parse(response.body)
-  #
-  #   expect(response).to be_successful
-  #   expect(invoice["id"]).to eq(id)
-  # end
+   it 'finds one invoice' do
+    merchant1 = Merchant.create!(name: "Ollivanders")
+    customer = Customer.create!(first_name: "Harry", last_name: "Potter")
+    invoice1 = Invoice.create!(status: "pending", merchant: merchant1 , customer: customer)
+    id = invoice1.id
+
+    get "/api/v1/invoices/#{id}"
+
+    invoice = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice["id"]).to eq(id)
+  end
 end
