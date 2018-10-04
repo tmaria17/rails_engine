315a3ff33eaf702bc21a88144e586ee95e64c532
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-04 14:24:47 -0600

    wip

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 require 'simplecov'
 require 'simplecov-console'
-SimpleCov.formatter = SimpleCov::Formatter::Console
 SimpleCov.start
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
