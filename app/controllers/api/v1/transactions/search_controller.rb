92a8dafe4c443c6d4929b366088ca5425981f6b7
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-04 14:12:48 -0600

    adds simplecov and routes for itransaction search, adds transaction serailizer.

---
+++ app/controllers/api/v1/transactions/search_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::Transactions::SearchController < ApplicationController
+  def show
+    render json: Transaction.find_by(search_params)
+  end
+
+  def index
+    render json: Transaction.where(search_params)
+  end
+
+
+
+  private
+
+    def search_params
+      params.permit(:id, :invoice_id, :credit_card_number, :result)
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
         get 'find', to: 'search#show'
         get 'find_all', to: 'search#index'
       end
+      namespace :transactions do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+      end
       resources :merchants, only:[:index, :show]
       resources :items, only:[:index, :show]
       resources :customers, only:[:index, :show]
