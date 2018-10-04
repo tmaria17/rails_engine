9ec0d11ae0dc863f4e5a40b8777f0360fdbeab0f
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-03 18:25:17 -0600

    Creates total revenue method for merchant.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,11 @@ class Merchant < ApplicationRecord
   has_many  :customers, through: :invoices
   validates_presence_of :name
 
-  def total_revenue
+  def self.total_revenue(limit)
+    .joins(invoice_items: [:transactions])
+    .where(transactions: {result: "success"})
+    .order("sum(invoice_items.quantity * invoice_items.unit_price) DESC")
+    .group(:id)
+    .limit(limit)
   end
 end
