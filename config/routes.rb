0cd4c6f1fae15d807d4c55fb8b9ec69c41f5f7eb
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-04 13:30:43 -0600

    Serch Controller for items wip.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
         get 'find_all', to: 'search#index'
         get 'most_items', to: 'most_items#index'
       end
+      namespace :items do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+      end
       resources :merchants, only:[:index, :show]
       resources :items, only:[:index, :show]
       resources :customers, only:[:index, :show]
