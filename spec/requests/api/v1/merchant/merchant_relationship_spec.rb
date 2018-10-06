c2e66c81e455d627701a535c6771fadff52f467c
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-06 12:26:10 -0600

    merchant relationship endpoint testing done. All tests passing.

--- spec/requests/api/v1/merchant/merchant_relationship_spec.rb
+++ spec/requests/api/v1/merchant/merchant_relationship_spec.rb
@@ -13,4 +13,18 @@ describe 'Merchant api relationships' do
 
     expect(items.count).to eq(20)
   end
+
+  it 'returns merchants invoices' do
+    merchant = create(:merchant)
+    customer = create(:customer)
+    create_list(:invoice, 10, merchant_id: merchant.id, customer_id: customer.id)
+
+    get "/api/v1/merchants/#{merchant.id}/invoices"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices.count).to eq(10)
+  end
 end
